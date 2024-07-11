32712121684b046b8f1ad053e3f4d2042c968d27
TyrWark <167569189+TyrWark@users.noreply.github.com>
2024-07-11 16:30:13 -0500

    Update Include.js

--- Include.js
+++ Include.js
@@ -1,21 +1,3 @@
-
-// ==UserScript==
-// @name         Diagnostic Links for BackOffice
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Adds links for easy access to Diag reports
-// @author       Ty Wark
-// @match        https://lightspeedanalytics.net/cl_accounts/settings
-// @match        https://app.lightspeedanalytics.net/embed/explore/*
-// @match        https://app.lightspeedanalytics.net/embed/*
-// @run-at      document-idle
-// @grant       GM_addStyle
-// ==/UserScript==
-
-//Documentation found here https://lightspeedhq.atlassian.net/wiki/spaces/CLOUD/pages/139441487/Looker+Diagnostic+Analytics+Reports
-/* globals jQuery, $, waitForKeyElements */
-
-
 var SubResp = new Object
 var ParseResp = new Array
 var table
